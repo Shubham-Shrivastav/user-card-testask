558769e38fbe4144f03411b97a4e7dca3fac1800
Shubham-Shrivastyav <shuboom969@gmail.com>
2024-03-19 22:09:30 +0530

    Add the timer functionlity to start and stop the clock, get the time zone form API.

--- user-card-testask/src/components/Timer/Timer.jsx
+++ user-card-testask/src/components/Timer/Timer.jsx
@@ -1,25 +1,59 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from "react";
+import axios from "axios";
 
-function Timer() {
+const Timer = () => {
     const [currentTime, setCurrentTime] = useState(new Date());
+    const [paused, setPaused] = useState(false);
+    const [countries, setCountries] = useState([]);
+    const [selectedCountry, setSelectedCountry] = useState('');
 
     useEffect(() => {
-        const timerID = setInterval(() => tick(), 1000);
+        const interval = setInterval(() => {
+            if (!paused) {
+                setCurrentTime(prevTime => new Date(prevTime.getTime() + 1000));
+            }
+        }, 1000);
 
-        return function cleanup() {
-            clearInterval(timerID);
-        };
-    });
+        return () => clearInterval(interval);
+    }, [paused]);
 
-    function tick() {
-        setCurrentTime(new Date());
-    }
+    useEffect(() => {
+        axios.get("http://worldtimeapi.org/api/timezone")
+            .then(response => {
+                setCountries(response.data);
+                setSelectedCountry(response.data[0]);
+            })
+            .catch(error => {
+                console.error("Error fetching countries and timezones:", error);
+            });
+    }, []);
+
+    const handleCountryChange = (event) => {
+        setSelectedCountry(event.target.value);
+    };
+
+    const handlePauseTimer = () => {
+        setPaused(prevPaused => !prevPaused);
+    };
+
+    const countryOptions = useMemo(() => {
+        return countries.map((country, index) => (
+            <option key={index} value={country}>{country}</option>
+        ));
+    }, [countries]);
 
     return (
         <div>
-            <div>{currentTime.toLocaleTimeString()}</div>
+            <select value={selectedCountry} onChange={handleCountryChange}>
+                {countryOptions}
+            </select>
+
+            <h2>Current Time: {currentTime && currentTime.toLocaleTimeString()}</h2>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white px-4 rounded" onClick={handlePauseTimer}>
+                {paused ? "Start" : "Pause"}
+            </button>
         </div>
     );
-}
+};
 
 export default Timer;
